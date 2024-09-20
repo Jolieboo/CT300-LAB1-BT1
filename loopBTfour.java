28857b719e1dc0add64f8988fb0357e8ec64ea17
Jolieboo <duyb2111789@student.ctu.edu.vn>
2024-09-20 16:36:40 +0700

    Cau 5 BT4 LAB1

---
+++ loopBTfour.java
@@ -0,0 +1,8 @@
+public class loopBTfour {
+    public static void main(String[] args) {
+        // Vòng lặp từ 1 đến 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
